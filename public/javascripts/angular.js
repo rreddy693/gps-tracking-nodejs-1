22a86fd7ba993be3157fbe198f3e3be8f745de86
mRo <markoknez@gmail.com>
2016-02-29 15:21:55 +0100

    ui refactoring

--- public/javascripts/angular.js
+++ public/javascripts/angular.js
@@ -1,4 +1,7 @@
 angular.module('app',[])
+	.run(function($rootScope){
+		$rootScope._ = _;
+	})
 	.config(function($sceProvider){
 		$sceProvider.enabled(false);
 	})
@@ -6,12 +9,25 @@ angular.module('app',[])
 		$http.get('http://ec2.mrostudios.com:3000/devices')
 			.then(function(response){
 				$scope.devices = response.data;
+				for(d in $scope.devices){
+					$scope.devices[d].locations.forEach(function(l){
+						l.lat /= 60;
+						l.lon /= 60;
+					});
+				}
 			});
 
-		$scope.getMapLink = function(device){
+		$scope.getIFrameLink = function(location){
 			return "https://www.google.com/maps/embed/v1/place?\
 						key=AIzaSyBJwFspRfvFCumitTo7RD-2yrjjMUyV4eg\
-						&q="+(device.lastLocation.lat/60)+","+(device.lastLocation.lon/60)+"\
+						&q="+location.lat+","+location.lon+"\
 						&zoom=18";
+		};
+
+		$scope.getMapLink = function(location){
+			return "https://maps.google.com/maps/place/\
+						"+location.lat+" "+location.lon+"\
+						/@"+location.lat+","+location.lon+",18z";
+						
 		};
 	}]);
